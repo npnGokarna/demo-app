1959ee27e947bf28df11be3114ca0e1a90c74a68
Gokarna Neupane <npn.gokarna@gmail.com>
2019-12-18 00:08:21 -0600

    react hook test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [names, fetchData] = useState([]);
+
+  useEffect(() => {
+    console.log(names);
+  });
+
+  function populateList() {
+    (() => {
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      fetch(url).then((res) => {
+        return res.json();
+      }).then((res) => {
+        fetchData(res);
+      }).catch((e) => {
+        console.log(e);
+      });
+    })();
+  }
+
+  function dePopulateList() {
+    fetchData([]);
+  }
+
+   
+  // const names = [1,2,3];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="listNames">{names.map((item) => <p key={item.id}>{item.name}</p>)}</div>
+        <button className="button" onClick={populateList}> populate list</button>
+        <button className="button" onClick={dePopulateList}> Clear list</button>
       </header>
     </div>
   );
